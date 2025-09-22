1fabf00f7a70035b708741b0e2aac79155d51dd2
Eshu <tjano.xibalba@gmail.com>
2025-09-22 23:19:49 +0900

    scalafmt tuning

--- stages/src/main/scala/h8io/xi/stages/OnDone.scala
+++ stages/src/main/scala/h8io/xi/stages/OnDone.scala
@@ -10,25 +10,26 @@ trait OnDone[-I, +O, +E] {
 
   def dispose(): Unit = {}
 
-  private[stages] def safe: OnDone.Safe[I, O, E] = new OnDone.Safe[I, O, E] {
-    def onSuccess(): State[I, O, E] =
-      try self.onSuccess()
-      catch { case e: Exception => State.Panic(e) }
+  private[stages] def safe: OnDone.Safe[I, O, E] =
+    new OnDone.Safe[I, O, E] {
+      def onSuccess(): State[I, O, E] =
+        try self.onSuccess()
+        catch { case e: Exception => State.Panic(e) }
 
-    def onComplete(): State[I, O, E] =
-      try self.onComplete()
-      catch { case e: Exception => State.Panic(e) }
+      def onComplete(): State[I, O, E] =
+        try self.onComplete()
+        catch { case e: Exception => State.Panic(e) }
 
-    def onError(): State[I, O, E] =
-      try self.onError()
-      catch { case e: Exception => State.Panic(e) }
+      def onError(): State[I, O, E] =
+        try self.onError()
+        catch { case e: Exception => State.Panic(e) }
 
-    def onPanic(): State[I, O, E] =
-      try self.onPanic()
-      catch { case e: Exception => State.Panic(e) }
+      def onPanic(): State[I, O, E] =
+        try self.onPanic()
+        catch { case e: Exception => State.Panic(e) }
 
-    override def dispose(): Unit = self.dispose()
-  }
+      override def dispose(): Unit = self.dispose()
+    }
 
   final def map[_I, _O, _E](f: State[I, O, E] => State[_I, _O, _E]): OnDone[_I, _O, _E] =
     new OnDone[_I, _O, _E] {
@@ -57,17 +58,18 @@ object OnDone {
   trait Safe[-I, +O, +E] extends OnDone[I, O, E] {
     self =>
 
-    final private[stages] def <~[_O, _E >: E](that: Safe[O, _O, _E]): Safe[I, _O, _E] = new Safe[I, _O, _E] {
-      def onSuccess(): State[I, _O, _E] = that.onSuccess() ~> self
-      def onComplete(): State[I, _O, _E] = that.onComplete() ~> self
-      def onError(): State[I, _O, _E] = that.onError() ~> self
-      def onPanic(): State[I, _O, _E] = that.onPanic() ~> self
+    final private[stages] def <~[_O, _E >: E](that: Safe[O, _O, _E]): Safe[I, _O, _E] =
+      new Safe[I, _O, _E] {
+        def onSuccess(): State[I, _O, _E] = that.onSuccess() ~> self
+        def onComplete(): State[I, _O, _E] = that.onComplete() ~> self
+        def onError(): State[I, _O, _E] = that.onError() ~> self
+        def onPanic(): State[I, _O, _E] = that.onPanic() ~> self
 
-      override def dispose(): Unit = {
-        that.dispose()
-        self.dispose()
+        override def dispose(): Unit = {
+          that.dispose()
+          self.dispose()
+        }
       }
-    }
 
     final override private[stages] def safe: Safe[I, O, E] = this
 
--- stages/src/main/scala/h8io/xi/stages/Yield.scala
+++ stages/src/main/scala/h8io/xi/stages/Yield.scala
@@ -17,10 +17,11 @@ sealed trait Yield[-I, +O, +E] {
 
 object Yield {
   final case class Some[-I, +O, +E](out: O, onDone: OnDone[I, O, E]) extends Yield[I, O, E] {
-    private[stages] def ~>[_O, _E >: E](stage: Stage[O, _O, _E]): Yield[I, _O, _E] = stage.safe(out) match {
-      case Some(_out, _onDone) => Some(_out, onDone.safe <~ _onDone.safe)
-      case None(_onDone) => None(onDone.safe <~ _onDone.safe)
-    }
+    private[stages] def ~>[_O, _E >: E](stage: Stage[O, _O, _E]): Yield[I, _O, _E] =
+      stage.safe(out) match {
+        case Some(_out, _onDone) => Some(_out, onDone.safe <~ _onDone.safe)
+        case None(_onDone) => None(onDone.safe <~ _onDone.safe)
+      }
 
     @inline def map[_I, _O >: O, _E >: E](f: OnDone[I, O, E] => OnDone[_I, _O, _E]): Some[_I, _O, _E] =
       Some(out, f(onDone))
