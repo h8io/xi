b08443b101dda3a896adaf42972b472b0ccfa6ff
Eshu <tjano.xibalba@gmail.com>
2025-09-24 00:12:11 +0900

    Exhaused GlobalCountdown should call dispose of the latest OnDone: test

--- stages/src/main/scala/h8io/xi/stages/std/GlobalCountdown.scala
+++ stages/src/main/scala/h8io/xi/stages/std/GlobalCountdown.scala
@@ -3,18 +3,12 @@ package h8io.xi.stages.std
 import h8io.xi.stages.{Stage, Yield}
 
 object GlobalCountdown {
-  private[stages] final case class Impl[-I, +O, +E](i: Long, stage: Stage[I, O, E], dispose: () => Unit)
-      extends Stage.Safe[I, O, E] {
-    def apply(in: I): Yield[I, O, E] =
-      if (i == 1) {
-        val `yield` = stage.safe(in)
-        `yield`.lift[I, O, E](_ => new DeadEnd(`yield`.onDone.dispose _))
-      } else if (i > 0) {
-        val `yield` = stage.safe(in)
-        `yield`.lift(Impl(i - 1, _, `yield`.onDone.dispose _))
-      } else new DeadEnd(dispose).Yield
+  private[stages] final case class Impl[-I, +O, +E](i: Long, stage: Stage[I, O, E]) extends Stage.Safe[I, O, E] {
+    def apply(in: I): Yield[I, O, E] = {
+      val `yield` = stage.safe(in)
+      `yield`.lift[I, O, E](if (i == 1) _ => new DeadEnd(`yield`.onDone.dispose _) else Impl(i - 1, _))
+    }
   }
 
-  def apply[I, O, E](n: Long, stage: Stage[I, O, E]): Stage.Safe[I, O, E] =
-    if (n > 0) Impl(n, stage, { () => }) else DeadEnd
+  def apply[I, O, E](n: Long, stage: Stage[I, O, E]): Stage.Safe[I, O, E] = if (n > 0) Impl(n, stage) else DeadEnd
 }
