e21933f98c9f2de9171b518495b8ea0ca5534c84
Eshu <tjano.xibalba@gmail.com>
2025-09-24 21:50:34 +0900

    Make timestamp getter mockable

--- stages/src/main/scala/h8io/xi/stages/util/LocalSoftClockdown.scala
+++ stages/src/main/scala/h8io/xi/stages/util/LocalSoftClockdown.scala
@@ -5,14 +5,15 @@ import h8io.xi.stages.{Stage, State, Yield}
 import scala.concurrent.duration.FiniteDuration
 
 object LocalSoftClockdown {
-  private final case class Head[-I, +O, +E](duration: Long, stage: Stage[I, O, E]) extends Stage.Safe[I, O, E] {
-    def apply(in: I): Yield[I, O, E] = stage.safe(in).lift(Tail(System.nanoTime(), this, _))
+  private[stages] final case class Head[-I, +O, +E](now: () => Long, duration: Long, stage: Stage[I, O, E])
+      extends Stage.Safe[I, O, E] {
+    def apply(in: I): Yield[I, O, E] = stage.safe(in).lift(Tail(now(), this, _))
   }
 
-  private final case class Tail[-I, +O, +E](ts: Long, head: Head[I, O, E], stage: Stage[I, O, E])
+  private[stages] final case class Tail[-I, +O, +E](ts: Long, head: Head[I, O, E], stage: Stage[I, O, E])
       extends Stage.Safe[I, O, E] {
     override def apply(in: I): Yield[I, O, E] =
-      if (System.nanoTime() - ts < head.duration) stage.safe(in).lift(Tail(ts, head, _))
+      if (head.now() - ts < head.duration) stage.safe(in).lift(Tail(ts, head, _))
       else Yield.None(State.Complete(head).onDone)
   }
 
@@ -23,5 +24,5 @@ object LocalSoftClockdown {
     apply(duration.toNanos, stage)
 
   @inline private def apply[I, O, E](duration: Long, stage: Stage[I, O, E]): Stage.Safe[I, O, E] =
-    if (duration > 0) Head(duration, stage) else DeadEnd
+    if (duration > 0) Head(System.nanoTime _, duration, stage) else DeadEnd
 }
