730e189e816a8c52b710052397e439d4e25c7c37
Eshu <tjano.xibalba@gmail.com>
2025-09-24 19:53:55 +0900

    Outcome could have effects (#18)

--- stages/src/main/scala/h8io/xi/stages/std/Repeat.scala
+++ stages/src/main/scala/h8io/xi/stages/std/Repeat.scala
@@ -7,7 +7,7 @@ import scala.annotation.tailrec
 final case class Repeat[-I, +O, +E](stage: Stage[I, O, E]) extends Stage.Safe[I, O, E] {
   override def apply(in: I): Yield[I, O, E] = {
     @tailrec def repeat(stage: Stage[I, O, E]): Yield[I, O, E] = {
-      val outcome = stage.safe(in).outcome
+      val outcome = stage.safe(in).outcome()
       outcome.state match {
         case State.Success(next) => repeat(next)
         case State.Complete(next) => outcome.toYield(State.Success(Repeat(next)))
