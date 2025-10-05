cfb53e67f2653301ad5803a05df2e99fc9fbc7b9
Eshu <tjano.xibalba@gmail.com>
2025-10-06 00:58:26 +0900

    More constructors

--- stages/src/main/scala/h8io/xi/stages/util/GlobalSoftDeadline.scala
+++ stages/src/main/scala/h8io/xi/stages/util/GlobalSoftDeadline.scala
@@ -3,18 +3,28 @@ package h8io.xi.stages.util
 import h8io.xi.stages
 import h8io.xi.stages.{Stage, State, Yield}
 
-import java.time.Instant
-import scala.math.Ordering.Implicits.*
+import java.time.{Duration, Instant}
+import scala.concurrent.duration.FiniteDuration
 
-final case class GlobalSoftDeadline[T](deadline: Instant) extends Stage.Endo[T, Nothing] {
-  self =>
+object GlobalSoftDeadline {
+  private[util] final case class Impl[T](now: () => Instant, deadline: Instant) extends Stage.Endo[T, Nothing] {
+    self =>
 
-  def apply(in: T): Yield.Some[T, T, Nothing] =
-    Yield.Some(in, if (deadline > Instant.now()) State.Success else State.Complete, OnDone)
+    def apply(in: T): Yield.Some[T, T, Nothing] =
+      Yield.Some(in, if (deadline.isAfter(now())) State.Success else State.Complete, OnDone)
 
-  private[util] case object OnDone extends stages.OnDone[T, T, Nothing] {
-    def onSuccess(): Stage[T, T, Nothing] = self
-    def onComplete(): Stage[T, T, Nothing] = self
-    def onError(): Stage[T, T, Nothing] = self
+    private[util] case object OnDone extends stages.OnDone[T, T, Nothing] {
+      def onSuccess(): Stage[T, T, Nothing] = self
+      def onComplete(): Stage[T, T, Nothing] = self
+      def onError(): Stage[T, T, Nothing] = self
+    }
   }
+
+  def apply[T](deadline: Instant): Stage.Endo[T, Nothing] = Impl(Instant.now _, deadline)
+
+  def apply[T](duration: FiniteDuration): Stage.Endo[T, Nothing] =
+    Impl(Instant.now _, Instant.now().plusNanos(duration.toNanos))
+
+  def apply[T](duration: Duration): Stage.Endo[T, Nothing] =
+    Impl(Instant.now _, Instant.now().plusNanos(duration.toNanos))
 }
