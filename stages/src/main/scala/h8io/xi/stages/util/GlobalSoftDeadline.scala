e785694c4cd3a2c5a7566c123325fa6779941f64
Eshu <tjano.xibalba@gmail.com>
2025-10-07 00:03:54 +0900

    GlobalSoftDeadline based on nanoseconds

--- stages/src/main/scala/h8io/xi/stages/util/GlobalSoftDeadline.scala
+++ stages/src/main/scala/h8io/xi/stages/util/GlobalSoftDeadline.scala
@@ -3,15 +3,16 @@ package h8io.xi.stages.util
 import h8io.xi.stages
 import h8io.xi.stages.{Stage, State, Yield}
 
-import java.time.{Duration, Instant}
+import java.time.Duration
 import scala.concurrent.duration.FiniteDuration
 
-private[util] final case class GlobalSoftDeadline[T](now: () => Instant, deadline: Instant)
-    extends Stage.Endo[T, Nothing] {
+private[util] final case class GlobalSoftDeadline[T](now: () => Long, duration: Long) extends Stage.Endo[T, Nothing] {
   self =>
 
+  private val ts: Long = now()
+
   def apply(in: T): Yield.Some[T, T, Nothing] =
-    Yield.Some(in, if (deadline.isAfter(now())) State.Success else State.Complete, OnDone)
+    Yield.Some(in, if (now() - ts >= duration) State.Success else State.Complete, OnDone)
 
   private[util] case object OnDone extends stages.OnDone[T, T, Nothing] {
     def onSuccess(): Stage[T, T, Nothing] = self
@@ -21,11 +22,8 @@ private[util] final case class GlobalSoftDeadline[T](now: () => Instant, deadlin
 }
 
 object GlobalSoftDeadline {
-  def apply[T](deadline: Instant): Stage.Endo[T, Nothing] = GlobalSoftDeadline(Instant.now _, deadline)
-
   def apply[T](duration: FiniteDuration): Stage.Endo[T, Nothing] =
-    GlobalSoftDeadline(Instant.now _, Instant.now().plusNanos(duration.toNanos))
+    GlobalSoftDeadline(System.nanoTime _, duration.toNanos)
 
-  def apply[T](duration: Duration): Stage.Endo[T, Nothing] =
-    GlobalSoftDeadline(Instant.now _, Instant.now().plus(duration))
+  def apply[T](duration: Duration): Stage.Endo[T, Nothing] = GlobalSoftDeadline(System.nanoTime _, duration.toNanos)
 }
