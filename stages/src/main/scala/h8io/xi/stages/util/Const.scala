7300cfc3ad83ca886a682a1f2d51e9cdacc01f21
Eshu <tjano.xibalba@gmail.com>
2025-10-07 23:23:25 +0900

    Const (#26)

---
+++ stages/src/main/scala/h8io/xi/stages/util/Const.scala
@@ -0,0 +1,16 @@
+package h8io.xi.stages.util
+
+import h8io.xi.stages
+import h8io.xi.stages.{Stage, State, Yield}
+
+final case class Const[+O](out: O) extends Stage[Any, O, Nothing] {
+  self =>
+
+  def apply(in: Any): Yield[Any, O, Nothing] = Yield.Some(out, State.Success, OnDone)
+
+  private[util] case object OnDone extends stages.OnDone[Any, O, Nothing] {
+    def onSuccess(): Stage[Any, O, Nothing] = self
+    def onComplete(): Stage[Any, O, Nothing] = self
+    def onError(): Stage[Any, O, Nothing] = self
+  }
+}
