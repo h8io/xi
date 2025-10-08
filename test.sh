8a6511f95127d3f383cc2f98680990f1512f5e1a
Eshu <tjano.xibalba@gmail.com>
2025-10-08 20:51:42 +0900

    Build all scoverage reports

--- test.sh
+++ test.sh
@@ -2,10 +2,4 @@
 
 set -euxo pipefail
 
-if [[ -n "${CI:-}" ]]; then
-  COVERAGE_REPORT=coverageSummary
-else
-  COVERAGE_REPORT=coverageAggregate
-fi
-
-sbt scalafmtSbtCheck scalafmtCheckAll +clean +coverage +test +$COVERAGE_REPORT
+sbt scalafmtSbtCheck scalafmtCheckAll +clean +coverage +test +coverageAggregate +coverageSummary
