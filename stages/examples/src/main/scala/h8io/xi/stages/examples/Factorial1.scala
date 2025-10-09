db6c9f5249067fe805cb20d5e2e522a7ad51e14a
Eshu <tjano.xibalba@gmail.com>
2025-10-09 22:45:45 +0900

    Factorial1

---
+++ stages/examples/src/main/scala/h8io/xi/stages/examples/Factorial1.scala
@@ -0,0 +1,18 @@
+package h8io.xi.stages.examples
+
+import h8io.xi.stages.util.{Const, Countdown, Loop}
+import h8io.xi.stages.{OnDone, Signal, Stage, Yield}
+
+object Factorial1 {
+  sealed class Agg(n: Long) extends Stage.Endo[BigInt, Nothing] with OnDone[BigInt, BigInt, Nothing] {
+    override def apply(in: BigInt): Yield.Some[BigInt, BigInt, Nothing] = Yield.Some(in * n, Signal.Success, this)
+
+    override def onSuccess(): Stage[BigInt, BigInt, Nothing] = new Agg(n + 1)
+    override def onComplete(): Stage[BigInt, BigInt, Nothing] = Agg
+    override def onError(): Stage[BigInt, BigInt, Nothing] = Agg
+  }
+
+  object Agg extends Agg(1)
+
+  def stage(n: Int): Stage[Unit, BigInt, Nothing] = Const(BigInt.apply(1)) ~> Loop(Agg ~> Countdown(n))
+}
---
+++ stages/examples/src/test/scala/h8io/xi/stages/examples/FactorialTest.scala
@@ -0,0 +1,29 @@
+package h8io.xi.stages.examples
+
+import h8io.xi.stages.Yield
+import org.scalacheck.Gen
+import org.scalatest.Inside
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+class FactorialTest extends AnyFlatSpec with Matchers with Inside with ScalaCheckPropertyChecks {
+  def factorial(n: Int): BigInt = (1 to n foldLeft BigInt(1))(_ * _)
+
+  "Factorial1" should "calculate factorial for positive arguments" in
+    forAll(Gen.choose(1, 1000)) { n =>
+      inside(Factorial1.stage(n)(())) { case Yield.Some(out, _, _) =>
+        println(s"$n! == $out")
+        out shouldBe factorial(n)
+      }
+    }
+
+  it should "not calculate factorial for zero" in {
+    Factorial1.stage(0)(()) should matchPattern { case Yield.None(_, _) => }
+  }
+
+  it should "not calculate factorial for negative arguments" in
+    forAll(Gen.choose(Int.MinValue, -1)) { n =>
+      Factorial1.stage(n)(()) should matchPattern { case Yield.None(_, _) => }
+    }
+}
