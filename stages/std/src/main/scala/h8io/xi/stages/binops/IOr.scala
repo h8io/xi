8707f82513ff22ab4942c36a766e44dfde2ddc84
Eshu <tjano.xibalba@gmail.com>
2025-10-12 00:39:36 +0900

    And (#39)

--- stages/std/src/main/scala/h8io/xi/stages/binops/IOr.scala
+++ stages/std/src/main/scala/h8io/xi/stages/binops/IOr.scala
@@ -22,8 +22,8 @@ final case class IOr[-I, +LO, +RO, +E](left: Stage[I, LO, E], right: Stage[I, RO
 object IOr {
   private final case class OnDone[-I, +LO, +RO, +E](left: stages.OnDone[I, LO, E], right: stages.OnDone[I, RO, E])
       extends stages.OnDone[I, Ior[LO, RO], E] {
-    override def onSuccess(): Stage[I, Ior[LO, RO], E] = IOr(left.onSuccess(), right.onSuccess())
-    override def onComplete(): Stage[I, Ior[LO, RO], E] = IOr(left.onComplete(), right.onComplete())
-    override def onError(): Stage[I, Ior[LO, RO], E] = IOr(left.onError(), right.onError())
+    def onSuccess(): Stage[I, Ior[LO, RO], E] = IOr(left.onSuccess(), right.onSuccess())
+    def onComplete(): Stage[I, Ior[LO, RO], E] = IOr(left.onComplete(), right.onComplete())
+    def onError(): Stage[I, Ior[LO, RO], E] = IOr(left.onError(), right.onError())
   }
 }
