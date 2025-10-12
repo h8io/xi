d597117b7ed6bf38811125f8b4e60d1d3c38c8f0
Eshu <tjano.xibalba@gmail.com>
2025-10-13 02:05:11 +0900

    Head test

--- stages/std/src/test/scala/h8io/xi/stages/decorators/LocalSoftDeadlineTest.scala
+++ stages/std/src/test/scala/h8io/xi/stages/decorators/LocalSoftDeadlineTest.scala
@@ -1,8 +1,8 @@
 package h8io.xi.stages.decorators
 
+import h8io.xi.stages.*
 import h8io.xi.stages.std.DeadEnd
-import h8io.xi.stages.{OnDone, Stage}
-import org.scalacheck.Gen
+import org.scalacheck.{Arbitrary, Gen}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
@@ -10,11 +10,17 @@ import org.scalatest.{Assertion, Inside}
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
 import java.time.Duration
+import java.util.UUID
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.FiniteDuration
 
 class LocalSoftDeadlineTest
-    extends AnyFlatSpec with Matchers with Inside with MockFactory with ScalaCheckPropertyChecks {
+    extends AnyFlatSpec
+    with Matchers
+    with Inside
+    with MockFactory
+    with ScalaCheckPropertyChecks
+    with StagesArbitraries {
   "LocalSoftDeadline" should "return DeadEnd if Scala duration is not positive" in
     forAll(Gen.choose(Long.MinValue, 0L)) { nanos =>
       val stage = mock[Stage[Any, Nothing, Nothing]]
@@ -31,6 +37,25 @@ class LocalSoftDeadlineTest
 
       test(LocalSoftDeadline(FiniteDuration(nanos, TimeUnit.NANOSECONDS), stage))
       test(LocalSoftDeadline(Duration.ofNanos(nanos), stage))
+    }
+
+  "Head" should "return yield Some" in
+    forAll(
+      Gen.zip(
+        Gen.long,
+        Gen.choose(1L, Long.MaxValue),
+        Arbitrary.arbitrary[String],
+        arbOnDoneToYield[String, UUID, Exception].arbitrary)) {
+      case (ts, duration, in, yieldSupplier) =>
+        val now = mock[() => Long]("now")
+        val stage = mock[Stage[String, UUID, Exception]]("underlying stage")
+        val `yield` = yieldSupplier(mock[OnDone[String, UUID, Exception]])
+        val head = LocalSoftDeadline.Head(now, duration, stage)
+        inSequence {
+          (now.apply _).expects().returns(ts)
+          (stage.apply _).expects(in).returns(`yield`)
+        }
+        head(in) shouldBe `yield`.mapOnDone(LocalSoftDeadline._OnDone(ts, now, duration, _))
     }
 
   "_OnDone" should "return Tail on success and Head on complete and on error" in
