6480c1efcdb49204c6085377e5f6860b85a7f2e6
Eshu <tjano.xibalba@gmail.com>
2025-10-13 01:30:45 +0900

    LocalSoftDeadline as a decorator

---
+++ stages/std/src/main/scala/h8io/xi/stages/decorators/LocalSoftDeadline.scala
@@ -0,0 +1,50 @@
+package h8io.xi.stages.decorators
+
+import h8io.xi.stages.std.DeadEnd
+import h8io.xi.stages.{OnDone, Signal, Stage, Yield}
+
+import scala.concurrent.duration.FiniteDuration
+
+trait LocalSoftDeadline[-I, +O, +E] extends Decorator[I, O, E]
+
+object LocalSoftDeadline {
+  private[decorators] final case class Head[-I, +O, +E](now: () => Long, duration: Long, stage: Stage[I, O, E])
+      extends LocalSoftDeadline[I, O, E] {
+    assume(duration > 0, s"Duration must be positive, got duration = $duration")
+
+    def apply(in: I): Yield[I, O, E] = {
+      val ts = now()
+      stage(in).mapOnDone(_OnDone(ts, now, duration, _))
+    }
+  }
+
+  private[decorators] final case class Tail[-I, +O, +E](
+      ts: Long,
+      now: () => Long,
+      duration: Long,
+      stage: Stage[I, O, E])
+      extends LocalSoftDeadline[I, O, E] {
+    def apply(in: I): Yield[I, O, E] =
+      if (now() - ts >= duration) Yield.None(Signal.Complete, OnDone.FromStage(Head(now, duration, stage)))
+      else stage(in).mapOnDone(Signal.Success, _OnDone(ts, now, duration, _))
+  }
+
+  private[decorators] final case class _OnDone[-I, +O, +E](
+      ts: Long,
+      now: () => Long,
+      duration: Long,
+      onDone: OnDone[I, O, E])
+      extends OnDone[I, O, E] {
+    def onSuccess(): Stage[I, O, E] = Tail(ts, now, duration, onDone.onSuccess())
+    def onComplete(): Stage[I, O, E] = Head(now, duration, onDone.onComplete())
+    def onError(): Stage[I, O, E] = Head(now, duration, onDone.onError())
+  }
+
+  def apply[I, O, E](duration: FiniteDuration, stage: Stage[I, O, E]): Stage[I, O, E] = apply(duration.toNanos, stage)
+
+  def apply[I, O, E](duration: java.time.Duration, stage: Stage[I, O, E]): Stage[I, O, E] =
+    apply(duration.toNanos, stage)
+
+  @inline private def apply[I, O, E](duration: Long, stage: Stage[I, O, E]): Stage[I, O, E] =
+    if (duration > 0) Head(System.nanoTime _, duration, stage) else DeadEnd
+}
