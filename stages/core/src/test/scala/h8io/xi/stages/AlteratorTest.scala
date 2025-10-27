13d666a7e9c55d6f6ff407d8800916438458cb9f
Eshu <tjano.xibalba@gmail.com>
2025-10-28 00:34:13 +0900

    Bugfix: value recursion

--- stages/core/src/test/scala/h8io/xi/stages/AlteratorTest.scala
+++ stages/core/src/test/scala/h8io/xi/stages/AlteratorTest.scala
@@ -6,10 +6,10 @@ import org.scalatest.matchers.should.Matchers
 
 class AlteratorTest extends AnyFlatSpec with Matchers with MockFactory {
   "Alterator's dispose" should "call underlying stage's dispose method" in {
-    val underlying = mock[Stage[Any, Nothing, Nothing]]
-    (underlying.dispose _).expects()
+    val stage = mock[Stage[Any, Nothing, Nothing]]
+    (stage.dispose _).expects()
     noException should be thrownBy new Decorator[Any, Nothing, Nothing] {
-      val alterand: Stage[Any, Nothing, Nothing] = alterand
+      val alterand: Stage[Any, Nothing, Nothing] = stage
       def apply(in: Any): Yield[Any, Nothing, Nothing] = throw new NoSuchMethodError
     }.dispose()
   }
